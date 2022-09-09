78aaee5a0d2f412214e53d573eb6ce5e606ab1e2
Glenn Daneels <glenndaneels@gmail.com>
2022-09-09 21:34:12 +0200

    Added more TODOs.

--- todo.mk
+++ todo.mk
@@ -2,3 +2,5 @@
 
 - Test adding a instruction to memory and reading it out in the run() function
 - Add SDL library support to CMakeLists 
+- Look into how CHIP 8 programs are formatted
+- Look into how to read CHIP8 programs in this C interpreter
