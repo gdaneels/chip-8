d22218e5fb10952b7ea0431c5127ec1179da654a
Glenn Daneels <glenndaneels@gmail.com>
2022-09-14 20:55:14 +0200

    Done checking how to add instruction to memory and read first nibble.

--- todo.mk
+++ todo.mk
@@ -1,6 +1,6 @@
 ## TODO
 
-- Test adding a instruction to memory and reading it out in the run() function
+- [DONE] Test adding a instruction to memory and reading it out in the run() function
 - Add SDL library support to CMakeLists 
 - Look into how CHIP 8 programs are formatted
 - Look into how to read CHIP8 programs in this C interpreter
