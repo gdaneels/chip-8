ea586eab18412b6bf0bccfdb6b9f26f201bfa00a
Glenn <glenndaneels@gmail.com>
2023-02-11 23:10:09 +0100

    Added new todos.

--- todo.mk
+++ todo.mk
@@ -1,9 +1,14 @@
 ## TODO
 
 - [DONE] Test adding a instruction to memory and reading it out in the run() function
-- Add SDL library support to CMakeLists 
+- [DONE] Add SDL library support to CMakeLists 
+- Create macro's to fetch first, second, third and forth nibbles of instruction
+	- Functions are probably better, but let's do a define as an exercise
 - Look into how CHIP 8 programs are formatted
 - Look into how to read CHIP8 programs in this C interpreter
 	- [DONE] Look if program really is in memory at correct place.
 	- Can you read first instruction correctly now (is it the first instruction of the IBM logo program?)
 	- Can we correctly handle possible failures when reading the program? How to catch this? (Now we simply do a return;)
+- Make generic graphics layer so SDL can be replaced with any other graphics library
+	- Will be handy if we port to ESP32
+- Get rid of nvim not found warnings/errors
