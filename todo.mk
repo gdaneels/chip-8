0a623703eb2f6274ba5e07a9c8650e077519503e
Glenn <glenndaneels@gmail.com>
2023-02-14 19:46:15 +0100

    Added new todos.

--- todo.mk
+++ todo.mk
@@ -8,14 +8,15 @@
 	- [DONE] Look if program really is in memory at correct place.
 	- [DONE] Can you read first instruction correctly now (is it the first instruction of the IBM logo program?)
 	- Can we correctly handle possible failures when reading the program? How to catch this? (Now we simply do a return;)
+- [DONE] Implement JUMP 1NNN instruction
 - Make generic graphics layer so SDL can be replaced with any other graphics library
 	- Will be handy if we port to ESP32
-- Get rid of nvim not found warnings/errors
+- [DONE] Get rid of nvim not found warnings/errors
 - Put built in font, as defines, together with other defines in interpreter.h
-- Implement JUMP 1NNN instruction
 - Generate debug information:
 	- Write PC
 	- Write instruction
 	- Write out all nibbles (to see that they're read correctly)
 - Run cmake differently so the compile_commands.json is always generated at the correct place
 	- I need to revise my cmake command
+- Make LOG macro's to do better debugging.
